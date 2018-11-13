c385a50598887a7562c372e6843cc2c70166534f
Jesper Stemann Andersen <jsa@ihp.dk>
2018-11-13 12:39:45 +0100

    Added methods for getting Pylon version

--- samples/init.jl
+++ samples/init.jl
@@ -1,4 +1,6 @@
 include(joinpath("..", "src", "wrapper.jl"))
 
+@show Wrapper.get_pylon_version()
+@show Wrapper.get_pylon_version_string()
 @show Wrapper.pylon_initialize()
 @show Wrapper.pylon_terminate(true)
--- src/pylon_wrapper.cpp
+++ src/pylon_wrapper.cpp
@@ -6,6 +6,13 @@ JLCXX_MODULE define_pylon_wrapper(jlcxx::Module& module)
 {
   using namespace Pylon;
 
+  module.method("get_pylon_version", []()
+  {
+    unsigned int major, minor, subminor, build;
+    GetPylonVersion(&major, &minor, &subminor, &build);
+    return std::make_tuple(major, minor, subminor, build);
+  });
+  module.method("get_pylon_version_string", &GetPylonVersionString);
   module.method("pylon_initialize", &PylonInitialize);
   module.method("pylon_terminate", &PylonTerminate);
 }
