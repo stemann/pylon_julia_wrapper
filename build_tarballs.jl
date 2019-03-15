f21252e9730c0344213b905a8d265762e8eda697
Jesper Stemann Andersen <jsa@ihp.dk>
2019-03-15 20:05:46 +0100

    Added Basler Pylon to build_tarballs.jl script

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -13,11 +13,28 @@ sources = [
     "src"
 ]
 
+pylon_version = "5.1.0.12682"
+# pylon_sha1sum = "db866bea9d8a8273d8b85cc331fa77b95bae4c83"
+
+basler_web_time_limit = round(Int, time()) + 24*60*60
+
 # Bash recipe for building across all platforms
-script = raw"""
-cd $WORKSPACE/srcdir
+script = """
+cd \$WORKSPACE/srcdir
+
+mkdir downloads && cd downloads
+
+curl https://www.baslerweb.com/fp-$basler_web_time_limit/media/downloads/software/pylon_software/pylon-$pylon_version-x86_64.tar.gz -O
+tar xfz pylon-$pylon_version-x86_64.tar.gz
+cd pylon-$pylon_version-x86_64
+tar xfz pylonSDK*.tar.gz
+cd ../..
+
+export PYLON_INCLUDE_PATH=\$WORKSPACE/srcdir/downloads/pylon-$pylon_version-x86_64/pylon5/include
+export PYLON_LIB_PATH=\$WORKSPACE/srcdir/downloads/pylon-$pylon_version-x86_64/pylon5/lib64
+
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_CXX_FLAGS="-march=x86-64" -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=${prefix} -DJulia_PREFIX=${prefix} ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DCMAKE_CXX_FLAGS="-march=x86-64" -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_FIND_ROOT_PATH=\$prefix -DJulia_PREFIX=\$prefix -DPYLON_INCLUDE_PATH=\${PYLON_INCLUDE_PATH} -DPYLON_LIB_PATH=\${PYLON_LIB_PATH} ..
 VERBOSE=ON cmake --build . --config Release --target install
 """
 
